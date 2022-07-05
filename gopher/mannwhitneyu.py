41c738fc15f8eac116efe64f014d1fcd274f0fdd
Carolyn Allen <Carolyn@Taluss-MacBook-Pro.local>
2022-07-05 15:09:45 -0700

    reformated code

--- gopher/mannwhitneyu.py
+++ gopher/mannwhitneyu.py
@@ -13,18 +13,14 @@ def tiecorrect(rankvals):
         arr = np.sort(np.ravel(rankvals[:, j]))
         idx = np.nonzero(
             np.concatenate(
-                (
-                    np.array([True]),
-                    arr[1:] != arr[:-1],
-                    np.array([True])
-                )
+                (np.array([True]), arr[1:] != arr[:-1], np.array([True]))
             )
         )[0]
         cnt = np.diff(idx).astype(np.float64)
 
         size = np.float64(arr.size)
         if size >= 2:
-            tc[j] = 1.0 - (cnt ** 3 - cnt).sum() / (size ** 3 - size)
+            tc[j] = 1.0 - (cnt**3 - cnt).sum() / (size**3 - size)
 
     return tc
 
@@ -52,7 +48,7 @@ def rankdata(data):
     return ranked
 
 
-def numba_mannwhitneyu(x, y, alternative='two-sided', use_continuity=True):
+def numba_mannwhitneyu(x, y, alternative="two-sided", use_continuity=True):
     """Version of Mann-Whitney U-test that runs in parallel on 2d arrays
     this is the asymptotic algo only
     """
@@ -66,7 +62,9 @@ def numba_mannwhitneyu(x, y, alternative='two-sided', use_continuity=True):
     # rank the data for the test
     ranked = rankdata(np.concatenate((x, y)))
     rankx = ranked[0:n1, :]  # get the x-ranks
-    u1 = n1 * n2 + (n1 * (n1 + 1)) / 2.0 - np.sum(rankx, axis=0)  # calc U for x
+    u1 = (
+        n1 * n2 + (n1 * (n1 + 1)) / 2.0 - np.sum(rankx, axis=0)
+    )  # calc U for x
     u2 = n1 * n2 - u1  # remainder is U for y
 
     # check for ties in the rankings
@@ -74,27 +72,26 @@ def numba_mannwhitneyu(x, y, alternative='two-sided', use_continuity=True):
 
     # if *everything* is identical we'll raise an error, not otherwise
     if np.all(T == 0):
-        raise ValueError('All numbers are identical in mannwhitneyu')
+        raise ValueError("All numbers are identical in mannwhitneyu")
 
     # get mean and standard deviation
     sd = np.sqrt(T * n1 * n2 * (n1 + n2 + 1) / 12.0)
     meanrank = n1 * n2 / 2.0 + 0.5 * use_continuity
 
     # do a one-sided or two-sided test depending on user specification
-    if alternative == 'greater':
+    if alternative == "greater":
         U, f = u2, 1
-    elif alternative == 'less':
+    elif alternative == "less":
         U, f = u1, 1
     else:
         U, f = np.maximum(u1, u2), 2
 
     # calculate z and use it to find the p-values
-    with np.errstate(divide='ignore', invalid='ignore'):
-        z = ((U - meanrank) / sd)
+    with np.errstate(divide="ignore", invalid="ignore"):
+        z = (U - meanrank) / sd
 
     p = stats.norm.sf(z)
     p *= f
-
     p = np.clip(p, 0, 1)
 
     return u1, p
