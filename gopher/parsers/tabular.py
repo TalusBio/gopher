0931862a9545b9b724b339f65cdd6067593d5b31
Lillian Tatka <tatkalillian@gmail.com>
2024-12-06 14:47:04 -0800

    add function to read and formatt diann pg files

--- gopher/parsers/tabular.py
+++ gopher/parsers/tabular.py
@@ -52,3 +52,30 @@ def read_metamorpheus(proteins_txt: str) -> pd.DataFrame:
         .fillna(0)
     )
     return proteins
+
+def read_diann(proteins_tsv: str) -> pd.DataFrame:
+    """
+    Reads a DIANN-generated TSV file containing protein information, processes it,
+    and returns a cleaned Pandas DataFrame with relevant data.
+
+    The function:
+    - Extracts the first protein accession from the "Protein.Ids" column to use as the DataFrame index.
+    - Renames the index axis to "Protein".
+    - Drops unnecessary metadata columns.
+
+    Args:
+        proteins_tsv (str): Path to the DIANN-generated TSV file.
+
+    Returns:
+        pd.DataFrame: A DataFrame with the processed protein data, indexed by the first protein accession.
+                      The returned DataFrame excludes the following columns:
+                      ["Protein.Group", "Protein.Ids", "Protein.Names", "Genes", "First.Protein.Description"].
+    """
+    proteins = pd.read_table(proteins_tsv)
+    accessions = proteins["Protein.Ids"].str.split(';').str[0]
+    
+    proteins = proteins.set_index(accessions)
+    proteins = proteins.rename_axis("Protein", axis="index")
+    return proteins.drop(
+        columns = ["Protein.Group", "Protein.Ids", "Protein.Names", "Genes", "First.Protein.Description"]
+    )
