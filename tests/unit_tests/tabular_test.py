8c173727366e2afc2a7f85ace2f14c181dcc3125
J. Sebastian Paez <jspaezp@gmail.com>
2025-03-27 09:51:16 -0700

    feat: efficient data read and pyproject cleanup

--- tests/unit_tests/tabular_test.py
+++ tests/unit_tests/tabular_test.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import pytest
 from io import StringIO
 from pandas.testing import assert_frame_equal
 
 from gopher.parsers.tabular import read_diann
+
 
 def test_read_diann_removes_metadata_and_sets_index():
     # Simulated DIANN output
@@ -16,12 +18,28 @@ PG2\tP23456\tProtein B\tGENE2\tDescription B\t1500\t2500
     # Expected DataFrame
     expected = pd.DataFrame(
         {
-            "Intensity.Sample1": [1000, 1500],
-            "Intensity.Sample2": [2000, 2500],
-        }, 
-        index=["P12345", "P23456"]
+            # The real diann data has float values in the intensities.
+            "Intensity.Sample1": [1000.0, 1500.0],
+            "Intensity.Sample2": [2000.0, 2500.0],
+        },
+        index=["P12345", "P23456"],
     )
     expected.index.name = "Protein"
 
     result = read_diann(mock_data)
     assert_frame_equal(result, expected)
+
+
+def test_read_diann_faile_with_gg():
+    # Simulated DIANN output
+    mock_data = StringIO(
+        """Genes\tFirst.Protein.Description\tIntensity.Sample1\tIntensity.Sample2
+GENE1\tDescription A\t1000\t2000
+GENE2\tDescription B\t1500\t2500
+"""
+    )
+
+    with pytest.raises(ValueError) as e:
+        result = read_diann(mock_data)
+
+    assert "Expected columns" in str(e.value.args[0])
